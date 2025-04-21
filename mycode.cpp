ae4d32b0c80cb1fb0293d6c1b64810659609293c
habtamu907 <habtamubiadiglign@gmail.com>
2025-04-21 16:08:39 +0300

    Update mycode.cpp

--- mycode.cpp
+++ mycode.cpp
@@ -1,1 +1,18 @@
+#include<iostream>
+#include<unordered_map>
+using namespace std;
+int main()
+   {
+unordered_map<string,int>login;
+string UserNmae;
+int PassWord;
+cout<<endl<< "Welcome to our Pesonal budget traker"<<endl;
+cout<<endl<<"Enter YOur Name of the account to registered"<<endl;
+getline(cin,UserNmae);
+cout<<endl<<"Enter Your PassWprd of the account to be registered"<<endl;
+cin>>PassWord;
+login["UserName"]=PassWord;
+cout<<endl<<"Your are successfully registered!";
+return 0;
+}
 
