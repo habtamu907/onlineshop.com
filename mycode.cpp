1f6514155f5c97b677f2b7775017103b3e6c302a
habtamu907 <habtamubiadiglign@gmail.com>
2025-04-22 10:01:16 +0300

    Update mycode.cpp

--- mycode.cpp
+++ mycode.cpp
@@ -4,14 +4,14 @@ using namespace std;
 int main()
    {
 unordered_map<string,int>login;
-string UserNmae;
+string UserName;
 int PassWord;
 cout<<endl<< "Welcome to our Pesonal budget traker"<<endl;
 cout<<endl<<"Enter YOur Name of the account to registered"<<endl;
-getline(cin,UserNmae);
+getline(cin,UserName);
 cout<<endl<<"Enter Your PassWprd of the account to be registered"<<endl;
 cin>>PassWord;
-login["UserName"]=PassWord;
+login[UserName]=PassWord;
 cout<<endl<<"Your are successfully registered!";
 return 0;
 }
